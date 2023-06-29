9264a7ab89f52f3b54aa2980b9f328614158799a
vertskater <vertskater@gmx.at>
2023-06-29 14:03:16 +0200

    finished projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,45 @@
 import data from "./data/content.json";
 import { useState, useEffect } from "react";
 import TextContent from "../ITextContent";
+import styles from "./css/projects.module.css";
 
 export default function Projects() {
+  const imgUrl = process.env.PUBLIC_URL;
   const [projects, setProjects] = useState<TextContent[]>([]);
   useEffect(() => {
     const content = data.find((item) => item.id === "projects");
     if (content) setProjects(content.projects as TextContent[]);
   }, []);
-
-  return <h1>This is the Projects Site</h1>;
+  console.log(imgUrl);
+  return (
+    <>
+      <section className={styles.container}>
+        {projects.map((project) => {
+          return (
+            <div key={project.id} className={styles.card}>
+              <div>
+                <img
+                  className={styles.image}
+                  src={`${imgUrl}/img/${project.image}`}
+                  alt={project.title}
+                />
+              </div>
+              <div className={styles.containerDescription}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <div>
+                  <a href={project.github} target="_blank">
+                    <i className="devicon-github-original-wordmark colored"></i>
+                  </a>
+                  <a href={project.link} target="_blank">
+                    Live Preview
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
 }
